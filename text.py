d9fca7917789d35b3863bd68218483c8a7332943
Caio Jorge Carvalho Lara <2022043744>
2023-01-19 09:49:05 -0300

    converterPDFtoTXT

--- text.py
+++ text.py
@@ -1,13 +1,15 @@
 import codecs
 import PyPDF2
- 
-pdf = open('colunamista.pdf','rb')
- 
-leitor_pdf = PyPDF2.PdfReader(pdf)
- 
-texto = ''
-for page in leitor_pdf.pages:
-    texto += page.extract_text()
 
-saida = codecs.open("colunamista.txt", "w", "utf-8")
-saida.write(texto)
+def converterPDFtoTXT(file):
+
+    pdf = open(file,'rb')
+ 
+    leitor_pdf = PyPDF2.PdfReader(pdf)
+ 
+    texto = ''
+    for page in leitor_pdf.pages:
+        texto += page.extract_text()
+
+    saida = codecs.open(file[:len(file)-4] + ".txt", "w", "utf-8")
+    saida.write(texto)
